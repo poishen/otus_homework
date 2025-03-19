02039ac5ed376405efd5e63fb12c4fa5ed51ef55
Shenev <shenevd@fizpribor.ru>
2025-03-19 19:09:42 +0000

    scripts/recovery.sh

---
+++ scripts/recovery.sh
@@ -0,0 +1,52 @@
+
+#!/bin/bash
+
+GIT_REPO="https://github.com/poishen/otus_homework.git"
+CLONE_DIR="/var/local/repo/"
+DB_NAME="MYBASE"
+DB_USER="user"
+DB_PASS="123456Aa"
+
+
+echo "Созданиие директорий"
+sudo mkdir /var/local/repo
+sudo mkdir /var/www/html/wordpress
+echo "Обновление пакетов"
+sudo apt update && sudo apt upgrade -y
+
+# Установка Apache2 и Nginx
+
+echo "Установка необходимых пакетов"
+sudo apt install -y apache2 nginx mysql-server php php-mysql libapache2-mod-php php-cli php-cgi php-gd unzip
+
+echo "Добавление в автозагрузку"
+sudo systemctl enable --now apache2 mysql-server nginx
+
+echo "Скачивание репозитория" 
+
+sudo git clone --branch master "$GIT_REPO" "$CLONE_DIR"
+
+echo "Настройка конфигов"
+echo "Копирование конфигов apache2."
+sudo cp /var/local/repo/apache2/apache2.conf /etc/apache2/apache2.conf
+echo "Копирование конфигов apache2.."
+sudo cp /var/local/repo/apache2/wordpress.conf /etc/apache2/sites-available/wordpress.conf
+echo "Копирование конфигов apache2..."
+sudo cp /var/local/repo/apache2/wordpress2.conf /etc/apache2/sites-available/wordpress2.conf
+echo "Копирование конфигов nginx"
+sudo cp /var/local/repo/nginx/default /etc/nginx/sites-available/deafult
+sudo cp /var/local/repo/nginx/nginx.conf /etc/nginx/nginx.conf
+echo "Копирование конфигов mysql"
+sudo cp /var/local/repo/mysql/mysqld_master.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+echo "Копирование crontab"
+sudo cp /var/local/repo/crontab/crontab /etc/crontab
+echo "Восстановление базы данных"
+sudo mysql < /var/local/repo/backups/backup_mysql.sql
+echo "Востановление файлов  cms"
+sudo tar -xzf /var/local/repo/backups/wp_backup.tar.gz -C /var/local/repo/backups/
+sudo cp -r /var/local/repo/backups/var/www/html/wordpress/* /var/www/html/wordpress/
+echo "Настройка прав для папок сайта"
+sudo chown -R www-data:www-data /var/www/html/wordpress
+sudo chmod -R 755 /var/www/html/wordpress
+
+echo "Все готово, не забудь внести изменения в файл hosts для подключения mysql-slave"
