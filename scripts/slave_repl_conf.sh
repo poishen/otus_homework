68a0713adb9a2d92720b71c008258ea9e9dcb71a
Shenev <shenevd@fizpribor.ru>
2025-03-21 17:34:19 +0000

    add slvae mysql script

---
+++ scripts/slave_repl_conf.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+GIT_REPO="https://github.com/poishen/otus_homework.git"
+CLONE_DIR="/var/local/repo/"
+MASTER_HOST=""
+MASTER_USER="replica"             # Репликационный пользователь на master
+MASTER_PASSWORD="123456Aa"     # Пароль для репликационного пользователя
+
+
+
+echo "Созданиие директорий"
+sudo mkdir /var/local/repo
+
+
+echo "Обновление пакетов"
+sudo apt update && sudo apt upgrade -y
+
+# Установка Apache2 и Nginx
+
+echo "Установка необходимых пакетов"
+sudo apt install -y mysql-server
+
+
+echo "Скачивание репозитория" 
+
+sudo git clone --branch master "$GIT_REPO" "$CLONE_DIR"
+
+
+echo "Копирование конфигов mysql"
+sudo cp /var/local/repo/mysql_slave/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+
+
+echo "Подключаюсь к мастер-серверу $MASTER_HOST..."
+sudo mysql -e "
+STOP SLAVE;
+CHANGE MASTER TO 
+  MASTER_HOST='$MASTER_HOST',
+  MASTER_USER='$MASTER_USER',
+  MASTER_PASSWORD='$MASTER_PASSWORD',
+  MASTER_AUTO_POSITION=1
+GET_SOURCE_PUBLIC_KEY = 1;
+START REPLICA;
+"
