071ac580b65cfefb407617e1ae53842156c7df93
Shenev <shenevd@fizpribor.ru>
2025-03-22 13:50:44 +0000

    d

--- scripts/prometheus_and_grafana.sh
+++ scripts/prometheus_and_grafana.sh
@@ -6,7 +6,7 @@ sudo apt install prometheus -y
 
 sudo apt-get install -y adduser libfontconfig1 musl
 wget https://dl.grafana.com/oss/release/grafana_10.0.3_amd64.deb
-sudo dpkg -i grafana-enterprise_11.5.2_amd64.deb
+sudo dpkg -i grafana_10.0.3_amd64.deb
 
 
 sudo systemctl daemon-reload
